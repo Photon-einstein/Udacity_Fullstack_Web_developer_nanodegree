097b86180eadded898685d34c71a739d1f7802ed
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2024-10-17 23:03:34 +0200

    Update test_flaskr.py

--- 2-Trivia_App/2-finish_code/backend/test_flaskr.py
+++ 2-Trivia_App/2-finish_code/backend/test_flaskr.py
@@ -230,12 +230,5 @@ class TriviaTestCase(unittest.TestCase):
             # Reestablish deleted entry
             res = self.client.post("/questions", json=self.deleted_question)
 
-    """
-    TODO DONE
-    Write at least one test for each test for successful operation and for expected errors.
-    """
-
-
-# Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
