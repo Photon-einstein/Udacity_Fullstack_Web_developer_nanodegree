d8f5c56da9676c239fc450004a83ad30ba46e95f
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2024-10-17 23:02:25 +0200

    Update __init__.py

--- 2-Trivia_App/2-finish_code/backend/flaskr/__init__.py
+++ 2-Trivia_App/2-finish_code/backend/flaskr/__init__.py
@@ -17,9 +17,6 @@ def create_app(db_URI="", test_config=None):
     else:
         setup_db(app)
 
-    """
-    @TODO DONE: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
     @app.after_request
     def after_request(response):
         response.headers.add("Access-Control-Allow-Origin",
@@ -32,11 +29,6 @@ def create_app(db_URI="", test_config=None):
         )
         return response
 
-    """
-    @TODO DONE:
-    Create an endpoint to handle GET requests
-    for all available categories.
-    """
     @app.route("/categories", methods=['GET'])
     def get_categories():
         try:
@@ -57,13 +49,6 @@ def create_app(db_URI="", test_config=None):
             # Handle specific error for invalid data types
             abort(404)
 
-    """
-    @TODO DONE:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
-    """
     def paginate_questions(request, selection):
         page = request.args.get("page", 1, type=int)
         start = (page - 1) * QUESTIONS_PER_PAGE
@@ -93,20 +78,6 @@ def create_app(db_URI="", test_config=None):
             # Handle specific error for invalid data types
             abort(404)
 
-    """
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
-    """
-
-    """
-    @TODO DONE:
-    Create an endpoint to DELETE question using a question ID.
-
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
-    """
     @app.route("/questions/<int:question_id>", methods=["DELETE"])
     def delete_question(question_id):
         try:
@@ -127,28 +98,6 @@ def create_app(db_URI="", test_config=None):
             )
         except:
             abort(422)
-
-    """
-    @TODO DONE:
-    Create an endpoint to POST a new question,
-    which will require the question and answer text,
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
-    """
-
-    """
-    @TODO DONE:
-    Create a POST endpoint to get questions based on a search term.
-    It should return any questions for whom the search term
-    is a substring of the question.
-
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
-    """
 
     @app.route("/questions", methods=["POST"])
     def create_questions():
@@ -203,15 +152,6 @@ def create_app(db_URI="", test_config=None):
         except:
             abort(422)
 
-    """
-    @TODO DONE:
-    Create a GET endpoint to get questions based on category.
-
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
-    """
-
     @app.route("/categories/<int:category>/questions", methods=['GET'])
     def retrieve_questions_by_category(category):
         try:
@@ -232,17 +172,7 @@ def create_app(db_URI="", test_config=None):
         except ValueError:
             # Handle specific error for invalid data types
             abort(404)
-    """
-    @TODO DONE:
-    Create a POST endpoint to get questions to play the quiz.
-    This endpoint should take category and previous question parameters
-    and return a random questions within the given category,
-    if provided, and that is not one of the previous questions.
 
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
-    """
     @app.route("/quizzes", methods=["POST"])
     def play_questions():
         body = request.get_json()
@@ -294,15 +224,14 @@ def create_app(db_URI="", test_config=None):
         except Exception as e:
             abort(422)
 
-    """
-    @TODO DONE:
-    Create error handlers for all expected errors
-    including 404 and 422.
-    """
-
+    
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+        return (
+            jsonify({"success": False, "error": 400,
+                    "message": "bad request"}),
+            400,
+        )
 
     @app.errorhandler(404)
     def not_found(error):
