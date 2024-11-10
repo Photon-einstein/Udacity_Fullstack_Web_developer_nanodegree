55fd0944257170204fcae6e196d6d8410f4f1a3d
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2024-11-10 03:44:48 +0100

    Update api.py

--- 3-Coffee_shop/backend/src/api.py
+++ 3-Coffee_shop/backend/src/api.py
@@ -25,25 +25,9 @@ def after_request(response):
     return response
 
 
-'''
-@TODO uncomment the following line to initialize the datbase
-!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
-!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
-!! Running this functon will add one
-'''
 # db_drop_and_create_all()
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
-
-
 @app.route("/drinks", methods=['GET'])
 def get_drinks():
     try:
@@ -62,17 +46,6 @@ def get_drinks():
     except ValueError:
         # Handle specific error for invalid data types
         abort(404)
-
-
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
-
 
 @app.route("/drinks-detail", methods=['GET'])
 @requires_auth('get:drinks-detail')
@@ -94,16 +67,6 @@ def get_drinks_detail(jwt):
         print(f"Error: {e}")
         abort(404)
 
-
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route("/drinks", methods=["POST"])
 @requires_auth('post:drinks')
 def create_drinks(jwt):
@@ -129,20 +92,6 @@ def create_drinks(jwt):
     except Exception as e:
         print(f"Error: {e}")
         abort(422)
-
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-
 
 @app.route("/drinks/<int:id>", methods=["PATCH"])
 @requires_auth('patch:drinks')
@@ -173,19 +122,6 @@ def update_drink(jwt, id):
         print(e)
         abort(422)
 
-
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
-
 @app.route("/drinks/<int:drink_id>", methods=["DELETE"])
 @requires_auth('delete:drinks')
 def delete_drink(jwt, drink_id):
@@ -207,11 +143,6 @@ def delete_drink(jwt, drink_id):
 
 
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-
-
 @app.errorhandler(400)
 def bad_request(error):
     return (
@@ -246,9 +177,3 @@ def internal_server_error(error):
                 "message": "internal server error"}),
         500,
     )
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
