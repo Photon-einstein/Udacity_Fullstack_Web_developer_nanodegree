5b7e8b8452e4f8dacc5a1b9a232ab80dde7e2d74
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2024-11-12 22:32:35 +0100

    Update environment.ts

--- 3-Coffee_shop/frontend/src/environments/environment.ts
+++ 3-Coffee_shop/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-n8zlv2hv8difjmzx.us', // the auth0 domain prefix
+    url: 'dev-xxxxxxxxxxxxxx.us', // the auth0 domain prefix
     audience: 'http://localhost:8100/get-drinks', // the audience set for the auth0 app
-    clientId: 'h7FkJpFj2oanklwvtYpbnhWOsxBTvMcZ', // the client id generated for the auth0 app
+    clientId: 'xxxxxxxxxxxxxxxxxxxx', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
